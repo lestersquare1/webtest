7aa15b904c59fa2f7cea8eac2aa0e641be10bf59
les young <lester.square@gmail.com>
2017-07-02 22:26:24 +1200

    add leeds classes and test

---
+++ my-app/src/main/java/com/webtest/app/PrintAnimals.java
@@ -0,0 +1,18 @@
+package com.webtest.app;
+
+/**
+ * Created by User on 2/07/2017.
+ */
+public class PrintAnimals {
+
+    public static void main( String[] args ) {
+        String[] Animals = new String[]{"Lion","Tiger","Cheetah", "Leopard", "Puma"};
+
+        for(int i=0;i<5;i++){
+            System.out.println("Animal " + Animals[i]);
+        }
+    }
+
+
+
+}
