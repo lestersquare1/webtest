8e549a265c0046029d00496f61822edf98faf039
les young <lester.square@gmail.com>
2017-07-02 21:59:51 +1200

    add leeds classes and test

---
+++ my-app/src/main/java/com/webtest/app/PrintDigits.java
@@ -0,0 +1,18 @@
+package com.webtest.app;
+
+/**
+ * Created by User on 2/07/2017.
+ */
+public class PrintDigits {
+
+    public static void main( String[] args ) {
+        String Digit = "Digit number";
+
+        for(int i=1;i<11;i++){
+            System.out.println(Digit + " " + i);
+        }
+    }
+
+
+
+}
